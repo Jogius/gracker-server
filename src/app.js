6f01ec3992879b1702ff73615a5ce052ad78b5b4
Jogius <julius@themakowskis.de>
2020-10-16 16:30:46 +0200

    Added express middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,13 @@ const express = require("express");
 const app = express();
 app.disable("x-powered-by");
 
+const cors = require("cors");
+app.use(cors({origin: process.env.ORIGIN_DOMAIN, credentials: true}));
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 const mongoose = require("mongoose");
 mongoose.connect(
   "mongodb://localhost/Gracker",
